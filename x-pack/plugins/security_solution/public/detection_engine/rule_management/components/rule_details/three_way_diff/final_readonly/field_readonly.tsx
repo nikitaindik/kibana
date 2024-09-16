7e3303be6a9b0781c555ae5371c7ecbd6d60ad0a
Nikita Indik <nikita.indik@elastic.co>
2024-09-16 18:31:03 +0200

    Try to type `FieldReadOnly` safely using Zod

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/final_readonly/field_readonly.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/final_readonly/field_readonly.tsx
@@ -6,93 +6,99 @@
  */
 
 import React from 'react';
-import type { DiffableAllFields } from '../../../../../../../common/api/detection_engine';
-import { KqlQueryReadOnly } from './fields/kql_query';
+import {
+  DiffableCommonFields,
+  DiffableCustomQueryFields,
+} from '../../../../../../../common/api/detection_engine';
+import type { DiffableRule } from '../../../../../../../common/api/detection_engine';
 import { DataSourceReadOnly } from './fields/data_source/data_source';
-import { EqlQueryReadOnly } from './fields/eql_query/eql_query';
-import { EsqlQueryReadOnly } from './fields/esql_query/esql_query';
-import { MachineLearningJobIdReadOnly } from './fields/machine_learning_job_id/machine_learning_job_id';
-import { RelatedIntegrationsReadOnly } from './fields/related_integrations/related_integrations';
-import { RequiredFieldsReadOnly } from './fields/required_fields/required_fields';
-import { SeverityMappingReadOnly } from './fields/severity_mapping/severity_mapping';
-import { RiskScoreMappingReadOnly } from './fields/risk_score_mapping/risk_score_mapping';
-import { ThreatMappingReadOnly } from './fields/threat_mapping/threat_mapping';
-import { ThreatReadOnly } from './fields/threat/threat';
-import { ThreatIndexReadOnly } from './fields/threat_index/threat_index';
-import { ThreatIndicatorPathReadOnly } from './fields/threat_indicator_path/threat_indicator_path';
-import { ThreatQueryReadOnly } from './fields/threat_query/threat_query';
 import { NameReadOnly } from './fields/name/name';
 import { TagsReadOnly } from './fields/tags/tags';
 import { DescriptionReadOnly } from './fields/description/description';
+import { assertUnreachable } from '../../../../../../../common/utility_types';
 
-interface FieldReadOnlyProps {
-  fieldName: keyof DiffableAllFields;
-  finalDiffableRule: DiffableAllFields;
-}
-
-export function FieldReadOnly({ fieldName, finalDiffableRule }: FieldReadOnlyProps) {
+function CustomQueryRuleFieldReadOnly({
+  fieldName,
+  finalDiffableRule,
+}: {
+  fieldName: keyof DiffableCustomQueryFields;
+  finalDiffableRule: DiffableCustomQueryFields;
+}) {
   switch (fieldName) {
     case 'data_source':
       return <DataSourceReadOnly dataSource={finalDiffableRule.data_source} />;
-    case 'description':
-      return <DescriptionReadOnly description={finalDiffableRule.description} />;
-    case 'eql_query':
-      return (
-        <EqlQueryReadOnly
-          eqlQuery={finalDiffableRule.eql_query}
-          dataSource={finalDiffableRule.data_source}
-        />
-      );
-    case 'esql_query':
-      return <EsqlQueryReadOnly esqlQuery={finalDiffableRule.esql_query} />;
-    case 'kql_query':
-      return (
-        <KqlQueryReadOnly
-          kqlQuery={finalDiffableRule.kql_query}
-          dataSource={finalDiffableRule.data_source}
-          ruleType={finalDiffableRule.type}
-        />
-      );
-    case 'machine_learning_job_id':
-      return (
-        <MachineLearningJobIdReadOnly
-          machineLearningJobId={finalDiffableRule.machine_learning_job_id}
-        />
-      );
+    // ... More custom query fields
+    default:
+      return assertUnreachable(fieldName);
+  }
+}
+
+function CommonRuleFieldsReadOnly({
+  fieldName,
+  finalDiffableRule,
+}: {
+  fieldName: keyof DiffableCommonFields;
+  finalDiffableRule: DiffableCommonFields;
+}) {
+  switch (fieldName) {
     case 'name':
       return <NameReadOnly name={finalDiffableRule.name} />;
-    case 'related_integrations':
-      return (
-        <RelatedIntegrationsReadOnly relatedIntegrations={finalDiffableRule.related_integrations} />
-      );
-    case 'required_fields':
-      return <RequiredFieldsReadOnly requiredFields={finalDiffableRule.required_fields} />;
-    case 'risk_score_mapping':
-      return <RiskScoreMappingReadOnly riskScoreMapping={finalDiffableRule.risk_score_mapping} />;
-    case 'severity_mapping':
-      return <SeverityMappingReadOnly severityMapping={finalDiffableRule.severity_mapping} />;
     case 'tags':
       return <TagsReadOnly tags={finalDiffableRule.tags} />;
-    case 'threat':
-      return <ThreatReadOnly threat={finalDiffableRule.threat} />;
-    case 'threat_index':
-      return <ThreatIndexReadOnly threatIndex={finalDiffableRule.threat_index} />;
-    case 'threat_indicator_path':
-      return (
-        <ThreatIndicatorPathReadOnly
-          threatIndicatorPath={finalDiffableRule.threat_indicator_path}
-        />
-      );
-    case 'threat_mapping':
-      return <ThreatMappingReadOnly threatMapping={finalDiffableRule.threat_mapping} />;
-    case 'threat_query':
-      return (
-        <ThreatQueryReadOnly
-          threatQuery={finalDiffableRule.threat_query}
-          dataSource={finalDiffableRule.data_source}
-        />
-      );
+    case 'description':
+      return <DescriptionReadOnly description={finalDiffableRule.description} />;
+    // ... More common fields
     default:
-      return null;
+      return assertUnreachable(finalDiffableRule);
+  }
+}
+
+interface FieldReadOnlyProps {
+  fieldName: string;
+  finalDiffableRule: DiffableRule;
+}
+
+export function FieldReadOnly({ fieldName, finalDiffableRule }: FieldReadOnlyProps) {
+  /* First try to handle common fields */
+  const isCommonFieldParseResult = DiffableCommonFields.keyof().safeParse(fieldName);
+
+  if (isCommonFieldParseResult.success) {
+    return (
+      <CommonRuleFieldsReadOnly
+        fieldName={isCommonFieldParseResult.data}
+        finalDiffableRule={finalDiffableRule}
+      />
+    );
+  }
+
+  /* If it's not a common field, then it's a type-specific field */
+  try {
+    switch (finalDiffableRule.type) {
+      case 'query':
+        return (
+          <CustomQueryRuleFieldReadOnly
+            fieldName={DiffableCustomQueryFields.keyof().parse(fieldName)}
+            finalDiffableRule={finalDiffableRule}
+          />
+        );
+      case 'saved_query':
+        return null;
+      case 'eql':
+        return null;
+      case 'esql':
+        return null;
+      case 'threat_match':
+        return null;
+      case 'threshold':
+        return null;
+      case 'machine_learning':
+        return null;
+      case 'new_terms':
+        return null;
+      default:
+        return assertUnreachable(finalDiffableRule);
+    }
+  } catch (error) {
+    return null;
   }
 }
